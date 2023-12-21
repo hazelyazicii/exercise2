4203b0c2f34bb8fe205a6a10319172043bd168a3
hazelyazicii <nazlikucukler@gmail.com>
2023-12-21 12:57:10 +0300

    Add project files.

---
+++ ödev1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Kodluyoruz Ödev
+
+Console.WriteLine("Hello");
+string answer;
+answer = Console.ReadLine();
+
+Console.WriteLine("Whats your name?");
+string name;
+name = Console.ReadLine();
+
+Console.WriteLine("Whats your surname?");
+string surname;
+surname = Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine("How old are you?");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Hello " + name + " " + surname);
+Console.Read();
